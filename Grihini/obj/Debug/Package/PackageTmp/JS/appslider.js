0e3d064dcdffa98d826ff8f33bf51517780e4b68
Debasish Halder <debasish@tangenttechsolutions.com>
2016-01-16 15:36:26 +0530

    Button Fix

--- Grihini/obj/Debug/Package/PackageTmp/JS/appslider.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    $('.sleekslider').sleekslider({
-        thumbs: [''],
-        labels: ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5'],
-        speed: 10000
-    });
-})
